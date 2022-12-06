03335455f1b541105fcb2ef256f5deab7b3b274d
Diego-Erquicia <diego.erquiciafundacionjala@gmail.com>
2022-12-06 00:15:45 -0400

    Clase 1 + tarea

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,4 +1,5 @@
 import pygame
+from dino_runner.components.dinosaur import Dinosaur
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
@@ -14,6 +15,7 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
@@ -30,18 +32,21 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
+        
 
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((255, 255, 255))#White
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
         image_width = BG.get_width()
-        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg)) #blit 
         self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
         if self.x_pos_bg <= -image_width:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
