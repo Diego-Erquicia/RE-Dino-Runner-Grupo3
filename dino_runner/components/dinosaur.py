9b20a8af8b3042ac8a9636dbabbac1a2627017c4
Diego-Erquicia <diego.erquiciafundacionjala@gmail.com>
2022-12-08 17:14:57 -0400

    clase 2 y 3

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -2,23 +2,27 @@ from pygame.sprite import Sprite
 import pygame
 
 from dino_runner.utils.constants import JUMPING, RUNNING, DUCKING
-X_POS = 80
-Y_POS = 310
-Y_POS_DUCK = 340
-JUMP_VELOCITY = 8
+
+
 class Dinosaur(Sprite):
+    X_POS = 80
+    Y_POS = 310
+    Y_POS_DUCK = 340
+    JUMP_VELOCITY = 8
 
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_POS
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+        self.dino_rect.y = self.Y_POS
         self.step_index = 0
 
         self.dino_run = True
         self.dino_jump = False
         self.dino_duck = False
-        self.jump_velocity = JUMP_VELOCITY
+        self.jump_velocity = self.JUMP_VELOCITY
 
     def update(self, user_input):
         if self.dino_run:
@@ -49,15 +53,15 @@ class Dinosaur(Sprite):
         self.dino_rect.y -= self.jump_velocity * 4
         self.jump_velocity -= 0.8
 
-        if self.jump_velocity < -JUMP_VELOCITY:
+        if self.jump_velocity < -self.JUMP_VELOCITY:
            self.dino_jump = False
-           self.dino_rect.y = Y_POS
-           self.jump_velocity = JUMP_VELOCITY
+           self.dino_rect.y = self.Y_POS
+           self.jump_velocity = self.JUMP_VELOCITY
 
     def duck(self):
         self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_POS_DUCK
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
         if self.step_index > 5:
             self.image = DUCKING[1]
         else:
@@ -66,8 +70,8 @@ class Dinosaur(Sprite):
 
     def run(self):
         self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_POS
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
         if self.step_index > 5:
             self.image = RUNNING[1]
         else:
---
+++ dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,17 @@
+import random
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+
+class SmallCactus(Obstacle):
+    def __init__(self, images):
+        self.type = random.randint(0,2)
+        super().__init__(images, self.type)
+        self.rect.y = 325
+
+    
+class LargeCactus(Obstacle):
+    def __init__(self, images):
+        self.type = random.randint(0, 2)
+        super(). __init__(images, self.type)
+        self.rect.y = 300
+
